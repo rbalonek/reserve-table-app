88752778191efbc01c07eb9ddf9d535dca4cd1c5
bob <robertbalonek@gmail.com>
2021-07-09 14:48:53 -0700

    Dev Mode back

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-const baseUrl = "https://reserve-table-proj.herokuapp.com/";
+// const baseUrl = "https://reserve-table-proj.herokuapp.com/";
+
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://reserve-table-proj.herokuapp.com/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
