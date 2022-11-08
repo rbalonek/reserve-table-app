d2062acef2db4e8fe84196e2ed69d0ddcd9977d5
Robert Balonek <robertbalonek@gmail.com>
2022-11-07 16:16:42 -0800

    Back to live

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const baseUrl = "https://reserve-table-proj.herokuapp.com/"
+// const baseUrl = "https://reserve-table-proj.herokuapp.com/" http://localhost:3000
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "reserve-table-db.fly.dev"
-    : "http://localhost:3000";
+    ? "https://reserve-table-proj.herokuapp.com/"
+    : "reserve-table-db.fly.dev";
 
 const api = axios.create({
   baseURL: baseUrl,
