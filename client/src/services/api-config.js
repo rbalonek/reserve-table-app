9b1188bd60185928749b031371c987d9983d0f78
Robert Balonek <robertbalonek@gmail.com>
2022-11-07 16:09:58 -0800

    Fly Io Setup

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const baseUrl = "https://reserve-table-proj.herokuapp.com/";
+// const baseUrl = "https://reserve-table-proj.herokuapp.com/"
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://reserve-table-proj.herokuapp.com/"
+    ? "reserve-table-db.fly.dev"
     : "http://localhost:3000";
 
 const api = axios.create({
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,50 +18,50 @@
 # # Guest.destroy_all
 
 
-# Table.create(table_number: "1", seat_count:5)
-# Table.create(table_number: "2", seat_count:5)
-# Table.create(table_number: "3", seat_count:5)
-# Table.create(table_number: "4", seat_count:5)
-# Table.create(table_number: "5", seat_count:5)
+Table.create(table_number: "1", seat_count:5)
+Table.create(table_number: "2", seat_count:5)
+Table.create(table_number: "3", seat_count:5)
+Table.create(table_number: "4", seat_count:5)
+Table.create(table_number: "5", seat_count:5)
 
-# Seat.create(seat_number: "1", table_id: 1)
-# Seat.create(seat_number: "2", table_id: 1)
-# Seat.create(seat_number: "3", table_id: 1)
-# Seat.create(seat_number: "4", table_id: 1)
-# Seat.create(seat_number: "5", table_id: 1)
+Seat.create(seat_number: "1", table_id: 1)
+Seat.create(seat_number: "2", table_id: 1)
+Seat.create(seat_number: "3", table_id: 1)
+Seat.create(seat_number: "4", table_id: 1)
+Seat.create(seat_number: "5", table_id: 1)
 
-# Seat.create(seat_number: "1", table_id: 2)
-# Seat.create(seat_number: "2", table_id: 2)
-# Seat.create(seat_number: "3", table_id: 2)
-# Seat.create(seat_number: "4", table_id: 2)
-# Seat.create(seat_number: "5", table_id: 2)
+Seat.create(seat_number: "1", table_id: 2)
+Seat.create(seat_number: "2", table_id: 2)
+Seat.create(seat_number: "3", table_id: 2)
+Seat.create(seat_number: "4", table_id: 2)
+Seat.create(seat_number: "5", table_id: 2)
 
-# Seat.create(seat_number: "1", table_id: 3)
-# Seat.create(seat_number: "2", table_id: 3)
-# Seat.create(seat_number: "3", table_id: 3)
-# Seat.create(seat_number: "4", table_id: 3)
-# Seat.create(seat_number: "5", table_id: 3)
+Seat.create(seat_number: "1", table_id: 3)
+Seat.create(seat_number: "2", table_id: 3)
+Seat.create(seat_number: "3", table_id: 3)
+Seat.create(seat_number: "4", table_id: 3)
+Seat.create(seat_number: "5", table_id: 3)
 
-# Seat.create(seat_number: "1", table_id: 4)
-# Seat.create(seat_number: "2", table_id: 4)
-# Seat.create(seat_number: "3", table_id: 4)
-# Seat.create(seat_number: "4", table_id: 4)
-# Seat.create(seat_number: "5", table_id: 4)
+Seat.create(seat_number: "1", table_id: 4)
+Seat.create(seat_number: "2", table_id: 4)
+Seat.create(seat_number: "3", table_id: 4)
+Seat.create(seat_number: "4", table_id: 4)
+Seat.create(seat_number: "5", table_id: 4)
 
-# Seat.create(seat_number: "1", table_id: 5)
-# Seat.create(seat_number: "2", table_id: 5)
-# Seat.create(seat_number: "3", table_id: 5)
-# Seat.create(seat_number: "4", table_id: 5)
-# Seat.create(seat_number: "5", table_id: 5)
+Seat.create(seat_number: "1", table_id: 5)
+Seat.create(seat_number: "2", table_id: 5)
+Seat.create(seat_number: "3", table_id: 5)
+Seat.create(seat_number: "4", table_id: 5)
+Seat.create(seat_number: "5", table_id: 5)
 
-# Guest.create(name:"Bob", email:"bob@bob.com", phone:'585-555-2210', seat_id:1 )
-# Guest.create(name:"Bill", email:"Bill@bob.com", phone:'585-555-0000', seat_id:6 )
-# Guest.create(name:"Jeff", email:"Jeff@bob.com", phone:'585-555-1111', seat_id:11 )
-# Guest.create(name:"Skylar", email:"Skylar@bob.com", phone:'585-555-2222', seat_id:16 )
-# Guest.create(name:"Goeff", email:"Goeff@bob.com", phone:'585-555-3333', seat_id:21 )
+Guest.create(name:"Bob", email:"bob@bob.com", phone:'585-555-2210', seat_id:1 )
+Guest.create(name:"Bill", email:"Bill@bob.com", phone:'585-555-0000', seat_id:6 )
+Guest.create(name:"Jeff", email:"Jeff@bob.com", phone:'585-555-1111', seat_id:11 )
+Guest.create(name:"Skylar", email:"Skylar@bob.com", phone:'585-555-2222', seat_id:16 )
+Guest.create(name:"Goeff", email:"Goeff@bob.com", phone:'585-555-3333', seat_id:21 )
 
 
 
-# puts "#{Table.count} Tables created!"
-# puts "#{Seat.count} Seats created!"
-# puts "#{Guest.count} Guests created!"
+puts "#{Table.count} Tables created!"
+puts "#{Seat.count} Seats created!"
+puts "#{Guest.count} Guests created!"
