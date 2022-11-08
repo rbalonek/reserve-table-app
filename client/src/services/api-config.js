455289d093af11a8e75a86db7975175ec74ddab7
Robert Balonek <robertbalonek@gmail.com>
2022-11-07 16:46:43 -0800

    Migrated to fly io

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -4,8 +4,8 @@ import axios from "axios";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://reserve-table-proj.herokuapp.com/"
-    : "reserve-table-db.fly.dev";
+    ? "https://reserve-table-db.fly.dev/"
+    : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
